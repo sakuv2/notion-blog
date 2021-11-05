6e7b984ee21a01f687d0cb1d6cfb1e44af92f812
sakuv2 <loots2438@gmail.com>
2021-11-06 02:48:30 +0900

    not found

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,9 @@
 const NotFound = () => {
   return (
-    <div className="flex debug debug-grid justify-center">
-      <div className="flex flex-column">
-        <span className={'db f0-ns mt6-ns'}>404</span>
-        <span className={'db tc'}>- Page Not Found -</span>
+    <div className="flex justify-center">
+      <div className="flex flex-column item-center">
+        <span className={'f1 f0-ns fw1 pa0 tc'}>404</span>
+        <span className={'tc gray ph1 bw1 pt2'}>- Page Not Found -</span>
       </div>
     </div>
   )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
+import 'tachyons'
 import '../styles/global.css'
 import 'katex/dist/katex.css'
-import 'tachyons'
 // import Footer from '../components/footer'
 
 export default function MyApp({ Component, pageProps }) {
